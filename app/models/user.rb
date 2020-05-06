d3b6ec38e5b1bb7a65f02fdde52db64107c6d91b
Cesar <cesarmota@ymail.com>
2020-05-05 21:46:18 -0500

    user show changes for delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :chatrooms
-    has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id",
-    has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id",
-    has_many :messages, through: :chatrooms,
+    has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id",dependent: :destroy
+    has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id",dependent: :destroy
+    has_many :messages, through: :chatrooms,dependent: :destroy
     has_many :posts
 
     validates :email,:name,uniqueness:{case_sensitive:false}
