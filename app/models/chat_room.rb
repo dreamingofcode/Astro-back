541854d6eed328fac031ed6da6b74994679bb546
Cesar <cesarmota@ymail.com>
2020-05-06 17:34:24 -0500

    user show changes for delete p97

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,6 +2,7 @@ class ChatRoom < ApplicationRecord
      belongs_to :users
     belongs_to :user1, :class_name => "User"
     belongs_to :user2, :class_name => "User"
+    has_many :messages
     has_many :user_one_messages, :class_name=> 'Message', :foreign_key=> "user_id"
     has_many :user_two_messages, :class_name=> 'Message', :foreign_key=> "recipient_id"
 
