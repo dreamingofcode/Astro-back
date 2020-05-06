7ba2932591a2a06b285dc20b70224f084e291ec8
Cesar <cesarmota@ymail.com>
2020-05-06 17:40:18 -0500

    user show changes for delete p97

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-    belongs_to :chat_room, :class_name=>"ChatRoom"
+    belongs_to :chatroom, :class_name=>"ChatRoom"
     belongs_to :user
     validates_length_of :text,  maximum: 210
 end
