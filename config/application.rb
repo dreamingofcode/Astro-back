ecdc2fa8c8201f9fa882848ed75ad67e92519174
Cesar <cesarmota@ymail.com>
2020-05-06 14:41:08 -0500

    attempts to fix user delete fetch

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,6 @@ module AstroConnectBanckends
         origins '*'
         resource '*',
                  :headers => :any,
-                :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
                  :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
