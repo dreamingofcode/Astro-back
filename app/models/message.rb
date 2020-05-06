b03c8f6efbf81ba985e5f6f1c69eb6b7e50c972e
Cesar <cesarmota@ymail.com>
2020-05-06 18:19:48 -0500

    xx

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
+    #currently table is DELETED
     # belongs_to :chat_rooms, :class_name=>"ChatRoom"
     # belongs_to :user
     # validates_length_of :text,  maximum: 210
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
      has_many :chat_rooms
-    # has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id"
-    # has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id"
-    # has_many :messages, through: :chat_rooms,dependent: :destroy
+    has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id"
+    has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id"
     has_many :posts
-
+    # has_many :messages, through: :chat_rooms,dependent: :destroy
+    
     validates :email,:name,uniqueness:{case_sensitive:false}
-    # validates :age,:numericality => { greater_than_or_equal_to: 18, less_than: 60 }
-    # post "/auth", to: 'auth#create'
-
-
-
 end
