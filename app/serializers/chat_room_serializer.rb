ab4a6459e09c945a90aa50559288a89fbee12132
Cesar <cesarmota@ymail.com>
2020-05-06 18:53:00 -0500

    dd

--- app/serializers/chat_room_serializer.rb
+++ app/serializers/chat_room_serializer.rb
@@ -1,6 +1,6 @@
 class ChatRoomSerializer < ActiveModel::Serializer
     attributes :id,:user1_id,:user2_id,:name
-    # has_many :user_one_messages
-    # has_many :user_two_messages
   end
+  # has_many :user_one_messages
+  # has_many :user_two_messages
   
