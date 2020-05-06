3fb6071ae3e24e9fc3487a5396f19421d0f2d0ee
Cesar <cesarmota@ymail.com>
2020-05-06 16:29:15 -0500

    attempts to fix user delete fetch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
-end
+    include ActionController::ImplicitRender
+    include ActionController::MimeResponds
+  
+    def cors_preflight_check
+      headers['Access-Control-Max-Age'] = '1728000'
+  
+      render json: {} # Render as you need
+    end
+  end
+  
+  
+  # config/application.rb
+  class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => 'POST, PUT, PATCH, DELETE, GET, OPTIONS',
+      'Access-Control-Request-Method' => '*',
+      'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    }
+  end
+  
+  
+  # config/routes.rb
+  # Last route definition code line
+  match '*path', to: 'application#cors_preflight_check', via: [:options]
--- config/application.rb
+++ config/application.rb
@@ -33,13 +33,13 @@ module AstroConnectBanckends
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*',
-    #              :headers => :any,
-    #              :methods => [:get, :post, :options, :delete, :put, :patch]
-    #   end
-    # end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 :headers => :any,
+                 :methods => [:get, :post, :options, :delete, :put, :patch]
+      end
+    end
   end
 end
