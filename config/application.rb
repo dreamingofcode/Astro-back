a4def89539d5f0494998cf6fe69d0c2f3805dcfe
Cesar <cesarmota@ymail.com>
2020-05-06 13:17:07 -0500

    user show changes for delete pt3

--- config/application.rb
+++ config/application.rb
@@ -33,13 +33,10 @@ module AstroConnectBanckends
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use Rack::Cors do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors, debug: true do
       allow do
         origins '*'
-        resource '*',
-                 :headers => :any,
-                 :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-                 :methods => [:get, :post, :options, :delete, :put, :patch]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
   end
