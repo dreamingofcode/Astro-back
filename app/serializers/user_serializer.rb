c8b3275f927ee6958d8f71aa38fa00c413ea876d
Cesar <cesarmota@ymail.com>
2020-05-06 17:14:44 -0500

    user show changes for delete p97

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
     attributes :id,:name,:email,:password,:bio,:birthDate,:gender,:sexualOrientation,:zodiac,:age,:profile_image
-  has_many :chatrooms
+  has_many :chat_rooms
     #  has_many :user_one_chatrooms
     #    has_many :user_two_chatrooms
        has_many :posts
