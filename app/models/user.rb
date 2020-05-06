dc5d1775d7233c0970b23909051e08bff8520c8c
Cesar <cesarmota@ymail.com>
2020-05-06 13:12:48 -0500

    user show changes for delete pt2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
-    # has_many :chatrooms
-    has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id",dependent: :destroy
-    has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id",dependent: :destroy
+    has_many :chatrooms, :foreign_key=> user1_id
+    # has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id",dependent: :destroy
+    # has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id",dependent: :destroy
     has_many :messages, through: :chatrooms,dependent: :destroy
     has_many :posts
 
