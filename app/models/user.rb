28d23ee8fa4c046730e00ed6b5c3d445f28e16d3
Cesar <cesarmota@ymail.com>
2020-05-06 18:47:41 -0500

    xx

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-     has_many :chat_rooms
+     has_many :chatrooms
     has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id"
     has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id"
     has_many :posts
