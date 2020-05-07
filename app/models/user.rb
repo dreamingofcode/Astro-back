dbcf251605e46f8b6ca6ab60804126c8d1a18395
Cesar <cesarmota@ymail.com>
2020-05-06 19:37:48 -0500

    ss

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :chat_rooms,:through=> :user_one_chatrooms && :user_two_chatrooms
+    has_many :chat_rooms,:through=> :user_one_chatrooms
+    has_many :chat_rooms,:through=> :user_two_chatrooms
     has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id"
     has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id"
     has_many :posts
