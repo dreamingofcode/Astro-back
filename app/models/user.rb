d12780d4c241e8c1eceda4d7787d599fe59ad381
Cesar <cesarmota@ymail.com>
2020-05-06 19:27:13 -0500

    ss

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
      has_many :chat_rooms
-    has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id"
-    has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id"
+    # has_many :user_one_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user1_id"
+    # has_many :user_two_chatrooms, :class_name => 'ChatRoom', :foreign_key => "user2_id"
     has_many :posts
     # has_many :messages, through: :chat_rooms,dependent: :destroy
     
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
     attributes :id,:name,:email,:password,:bio,:birthDate,:gender,:sexualOrientation,:zodiac,:age,:profile_image
    has_many :chat_rooms
-     has_many :user_one_chatrooms, :through=> :chat_rooms
-       has_many :user_two_chatrooms,:through=> :chat_rooms
+    #  has_many :user_one_chatrooms, :through=> :chat_rooms
+    #    has_many :user_two_chatrooms,:through=> :chat_rooms
        has_many :posts
        
   end
